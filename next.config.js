a5d4b0dbca38d80759bd8be71cb53cbf12ccb7d3
Ivan Cavero <ivangonzalezcavero@gmail.com>
2023-12-07 20:42:24 +0100

    Add million to improve performance

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
-/** @type {import('next').NextConfig} */
+const million = require('million/compiler')
+
 const nextConfig = {
   images: {
     domains: ['blitz-cdn.blitz.gg', 'cruip-tutorials.vercel.app']
-  }
+  },
+  reactStrictMode: true
 }
 
-module.exports = nextConfig
+const millionConfig = {
+  auto: true
+  // if you're using RSC:
+  // auto: { rsc: true },
+}
+
+module.exports = million.next(nextConfig, millionConfig)
