ec7dc1d2ac711bcf464edf006f7972b27519b4cc
Ivan Cavero <ivangonzalezcavero@gmail.com>
2023-12-07 20:48:32 +0100

    fix deploy

--- next.config.js
+++ next.config.js
@@ -1,16 +1,8 @@
-const million = require('million/compiler')
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     domains: ['blitz-cdn.blitz.gg', 'cruip-tutorials.vercel.app']
-  },
-  reactStrictMode: true
+  }
 }
 
-const millionConfig = {
-  auto: true
-  // if you're using RSC:
-  // auto: { rsc: true },
-}
-
-module.exports = million.next(nextConfig, millionConfig)
+module.exports = nextConfig
