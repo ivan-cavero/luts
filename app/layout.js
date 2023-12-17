7fa6471381d0a40f25c8daa3d2164143f602f8f2
Ivan Cavero <ivangonzalezcavero@gmail.com>
2023-12-17 17:23:02 +0100

    Try use client metrics

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from 'next/font/google'
 import '../css/globals.css'
+import { AxiomWebVitals } from 'next-axiom'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,6 +12,7 @@ export const metadata = {
 export default function RootLayout ({ children }) {
   return (
     <html lang="en">
+      <AxiomWebVitals />
       <body className={inter.className}>{children}</body>
     </html>
   )
